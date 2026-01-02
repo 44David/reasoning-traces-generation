e9e543db2af0066d0506cce32f36c6abd18c995a
44David <david.soane@outlook.com>
2026-01-02 10:37:15 -0600

    modify inference script to use different dataset

--- inference.py
+++ inference.py
@@ -1,56 +1,41 @@
-import json
-# from vllm import LLM, SamplingParams
-from datasets import load_dataset
-from tqdm import tqdm 
-import sys
+import torch
+from transformers import AutoTokenizer, AutoModelForCausalLM, GenerationConfig
+from tqdm import tqdm
 
-def main():
+model_name = "deepseek-ai/deepseek-math-7b-instruct"
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = AutoModelForCausalLM.from_pretrained(model_name, torch_dtype=torch.bfloat16, device_map="auto")
+model.generation_config = GenerationConfig.from_pretrained(model_name)
+model.generation_config.pad_token_id = model.generation_config.eos_token_id
 
+ds = load_dataset("openai/gsm8k". "main")
 
-    ds = load_dataset("qwedsacf/competition_math")
-            
-    samples_to_process = 12499
+num_samples = len(ds)
+
+progress_bar = tqdm(total=num_samples)
+
+# create data file if not already created
+with open(output_file, "w") as f:
+    pass
+
+for i in tqdm(range(num_samples)):
+    messages = [
+        {"role": "user", "content": f"{ds['train']['question'][i]}\nPlease reason step by step, and put your final answer within \\boxed{}."}
+    ]
+
+    input_tensor = tokenizer.apply_chat_template(messages, add_generation_prompt=True, return_tensors="pt")
+    outputs = model.generate(input_tensor.to(model.device), max_new_tokens=100)
+
+    result = tokenizer.decode(outputs[0][input_tensor.shape[1]:], skip_special_tokens=True)
+
+    trace = {
+        'index': i
+        "problem": ds['train']['question'][i],
+        "reasoning": result 
+    }
     
-    output_file = "SCoTD-deepseek-math-v2"   
-
-    llm = LLM(model="deepseek-ai/deepseek-math-7b-instruct")
-
-    sampling_params = SamplingParams(
-        temperature=0.8,
-        max_tokens=1024,
-        top_p=0.9
-    )
-
-    progress_bar = tqdm(total=samples_to_process)
-
-    for i in range(0, samples_to_process, 1):
-        prompt = f"{ds['train']['problem'][i]} \n Please reason step by step, and put your final answer within \\\\boxed{{}}."
-
-        outputs = llm.generate([prompt] * 6, sampling_params)
-
-        solutions = []
-        for output in outputs:
-            full_response = output.outputs[0].text
-            
-            # check if output prompt is too short or doesn't contain an actual answer.
-            while (len(full_response) < 5 or "\\\\boxed" not in full_response):
-                out = llm.generate(prompt, sampling_params)
-                full_response = out.outputs[0].text
-            
-            solutions.append(full_response)
-
-
-        data_point = {
-            "problem": ds["train"]["problem"][i],
-            "thinking_traces": solutions,
-            "correct_answer": ds["train"]["solution"][i],
-            "subject": ds["train"]["type"][i],
-            "level": ds["train"]["level"][i]
-        }
-
-        with open(output_file, "a") as f:
-            f.write(json.dumps(data_point) + "\n")
-
-        progress_bar.update(1)
+    with open(output_file, "a") as f:
+        f.write(json.dumps(trace) + "\n")
+        
+    progress_bar.update(1)
     
-main()
